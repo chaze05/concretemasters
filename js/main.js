8cf7c4fd4d62cbbe0b6c182c93036468cc90c848
rani <rani@spin.cw>
2021-08-31 21:36:13 +0800

    new sections

--- js/main.js
+++ js/main.js
@@ -1,5 +1,54 @@
 $(function(){
-    $('.owl-carousel').owlCarousel({
+ 
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+        console.log(scroll);
+        if (scroll > 50) {
+            //clearHeader, not clearheader - caps H
+            $("header").addClass("bgWhite");3 
+        }else{
+            $("header").removeClass("bgWhite");
+        }
+    }); 
+    
+    $('#projects').owlCarousel({
+        loop:true,
+        dots:true,
+        nav:true,
+        items:2,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:2
+            }
+        }
+    });
+
+    $('#services').owlCarousel({
+        loop:true,
+        dots:false,
+        nav:false,
+        items:2,
+        margin:10,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:2
+            }
+        }
+    });
+
+    $('#banner').owlCarousel({
         loop:true,
         dots:false,
         nav:false,
@@ -14,5 +63,5 @@ $(function(){
                 items:1
             }
         }
-    })
+    });
 })
